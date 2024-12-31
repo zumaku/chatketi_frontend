a50065e299fa96c24159a776c00a33c853726f78
zumaku <60200121101@uin-alauddin.ac.id>
2024-12-31 12:21:40 +0800

    feat: add tailwind-scrollbar package; enhance Chat component with auto-scroll and improved message handling

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { icon } from "../assets";
 import { CgArrowUpO } from "react-icons/cg";
-import axios from 'axios';
+import axios from "axios";
 
 export default function Chat() {
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {
+      type: "bot",
+      text: "Selamat datang! Saya senang dapat membantu Anda memahami pedoman karya tulis ilmiah ini. Apa yang Anda ingin tahu tentang pedoman ini?",
+    },
+    // { type: "user", text: "Selamat datang!" },
+  ]);
   const [input, setInput] = useState("");
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleSend = async () => {
     if (input.trim() === "") return;
@@ -14,13 +25,17 @@ export default function Chat() {
     setMessages([...messages, userMessage]);
 
     try {
-      const response = await axios.post("http://127.0.0.1:8000/query", {
-        query: input,
-      }, {
-        headers: {
-          "Content-Type": "application/json",
+      const response = await axios.post(
+        "http://127.0.0.1:8000/query",
+        {
+          query: input,
         },
-      });
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const botMessage = { type: "bot", text: response.data.result };
       setMessages((prevMessages) => [...prevMessages, botMessage]);
     } catch (error) {
@@ -30,8 +45,12 @@ export default function Chat() {
     setInput("");
   };
 
-    return (
-    <main className="container mx-auto flex h-1/2 justify-between items-center px-4">
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  return (
+    <main className="container mx-auto flex justify-between items-center px-4">
       {/* PDF Viewer */}
       <div className="w-1/2">
         <h1 className="font-head text-xl mb-2">
@@ -46,35 +65,44 @@ export default function Chat() {
         />
       </div>
 
-      <div className="w-1/2 p-5 flex flex-col h-screen justify-between overflow-y-scroll">
+      <div className="w-1/2 p-5 flex flex-col h-screen justify-start">
         {/* Chat */}
-        <div className="flex flex-col space-y-4 h-full">
-            <h1 className="font-head text-xl mb-2">Chat</h1>
-            {messages.map((message, index) => (
-                <div
-                    key={index}
-                    className={`flex space-x-4 items-start justify-${
-                        message.type === "user" ? "end" : "start"
-                    }`}
-                >
-                    {message.type === "bot" && <img src={icon} alt="" />}
-                    <p
-                        className={`max-w-9/12 p-2.5 rounded-lg ${
-                            message.type === "user" ? "bg-[#62C3A6] text-black text-end" : "bg-gray-200 text-black"
-                        }`}
-                    >
-                        {message.text}
-                    </p>
-                </div>
-            ))}
+        <h1 className="font-head text-xl mb-3">Chat</h1>
+        <div className="h-[90%] pr-4 pb-4 overflow-y-scroll">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`flex space-x-4 items-start mb-2 justify-${
+                message.type === "user" ? "end" : "start"
+              }`}
+            >
+              {message.type === "bot" && <img src={icon} alt="" />}
+              <p
+                className={`max-w-9/12 p-2.5 rounded-lg ${
+                  message.type === "user"
+                    ? "bg-[#62C3A6] text-black text-end"
+                    : "bg-gray-200 text-black"
+                }`}
+              >
+                {message.text}
+              </p>
+            </div>
+          ))}
+
+          <div ref={messagesEndRef} />
         </div>
-        <div className="flex flex-row w-full border rounded-lg bg-white shadow-xl">
+        <div className="flex flex-row w-full border border-gray-600 rounded-lg bg-white shadow-xl">
           <input
             type="text"
-            className="font-body font-semibold text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-0 block w-full p-2.5"
+            className="font-body font-semibold text-gray-900 text-sm rounded-lg focus:outline-none focus:ring-0 block w-full p-2.5 placeholder:text-gray-700"
             placeholder="Ketik di sini"
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSend();
+              }
+            }}
           />
           <button className="m-2" onClick={handleSend}>
             <CgArrowUpO size={28} className="text-accent" />
